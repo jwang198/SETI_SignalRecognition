571e8d21d74abd3e4ee0b55766414a56d3bc68cf
Jason Wang <jwang198@stanford.edu>
2016-05-23 19:25:37 -0700

    99.6% accuracy

--- classifier.R
+++ classifier.R
@@ -4,13 +4,16 @@ library(ROCR)
 #read data
 raw_squiggle <- read.table("/Users/Jason/Desktop/SETI_TimeSeries/DATA/official_ts_dataset_dft.csv", header=TRUE, sep=",", row.names="id")
 raw_nonsquiggle <- read.table("/Users/Jason/Desktop/SETI_TimeSeries/DATA/official_ts_dataset_nonsquiggle_dft.csv", header=TRUE, sep=",", row.names="id")
-#add labels to the data
-squiggle <- cbind(squiggle,T)
-nonsquiggle <- cbind(nonsquiggle,F)
+
+raw_squiggle <- cbind(raw_squiggle, label = T)
+raw_nonsquiggle <- cbind(raw_nonsquiggle, label = F)
+
 #put all the data together
-data <- rbind(squiggle, nonsquiggle)
-data <- scale(data)
+data <- rbind(raw_squiggle, raw_nonsquiggle)
+#scale data
+data[,1:(ncol(data)-1)] <- scale(data[,1:(ncol(data)-1)]) #SCALE AFTER CONCATENATING
 data[,1:5] <- data[,1:5]*sqrt(63) 
+
 #randomize the dataset
 data.randomized <- data[sample(nrow(data)), ]
 
@@ -44,6 +47,7 @@ train.pred <- prediction(train.fit, train.labels)
 train.roc <- performance(train.pred, measure = "tpr", x.measure = "fpr")
 plot(train.roc,colorize=FALSE, col="black")
 lines(c(0,1),c(0,1),col = "gray", lty = 4 )
+performance(train.pred, measure = "auc")@y.values
 
 #test set performance
 test.fit <- predict(lasso.cv,newx=data.matrix(test.indep),s="lambda.1se",type = "response")
@@ -53,26 +57,16 @@ test.pred <- prediction(test.fit, test.labels)
 test.roc <- performance(test.pred, measure = "tpr", x.measure = "fpr")
 plot(test.roc,colorize=FALSE, col="black")
 lines(c(0,1),c(0,1),col = "gray", lty = 4 )
+performance(test.pred, measure = "auc")@y.values
 
-#simple logistic
+#simple logistic: baseline?
+
 glm.fit <- glm(as.factor(train.labels) ~ ., data = data.frame(train.indep), family = binomial())
-glm.probs <- predict.glm(glm.fit, newdata = data.frame(train.indep), type = "response")
-glm.train.pred = prediction(glm.probs, train.labels)
-glm.train.roc <- performance(glm.train.pred, measure = "tpr", x.measure = "fpr")
-plot(glm.train.roc,colorize=FALSE, col="black")
+glm.probs <- predict.glm(glm.fit, newdata = data.frame(test.indep), type = "response")
+glm.test.pred = prediction(glm.probs, test.labels)
+glm.test.roc <- performance(glm.test.pred, measure = "tpr", x.measure = "fpr")
+plot(glm.test.roc,colorize=FALSE, col="black")
 lines(c(0,1),c(0,1),col = "gray", lty = 4 )
-plot(performance(glm.train.pred, measure = "acc")) #Varying by cutoff
 
-performance(glm.train.pred, measure = "auc")@y.values #True positive v.s. False positive (AUC of TPR v.s. FPR plot)
-
-
-#logistic using only modulation
-glm.fit <- glm(as.factor(train.labels) ~ ., data = data.frame(train.indep[,"modulation"]), family = binomial())
-glm.probs <- predict.glm(glm.fit, newdata = data.frame(train.indep[,"modulation"]), type = "response")
-glm.train.pred = prediction(glm.probs, train.labels)
-glm.train.roc <- performance(glm.train.pred, measure = "tpr", x.measure = "fpr")
-plot(glm.train.roc,colorize=FALSE, col="black")
-lines(c(0,1),c(0,1),col = "gray", lty = 4 )
-plot(performance(glm.train.pred, measure = "acc")) #Varying by cutoff
-
-performance(glm.train.pred, measure = "auc")@y.values #True positive v.s. False positive (AUC of TPR v.s. FPR plot)
+performance(glm.test.pred, measure = "auc")@y.values #True positive v.s. False positive (AUC of TPR v.s. FPR plot)
+plot(performance(glm.test.pred, measure = "acc")) #Varying by cutoff
