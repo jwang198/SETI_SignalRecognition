d97245e52e68de178c30e5bdc7ec7697dc028261
Jason Wang <jwang198@stanford.edu>
2016-05-23 17:34:53 -0700

    DATASET COMPLETE

--- feature_extraction_nonsquiggle.py
+++ feature_extraction_nonsquiggle.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 
 data1 = "/Users/Jason/Desktop/SETI_TimeSeries/DATA/nonsquiggle_ts.txt"
-#data2 = "/Users/Jason/Desktop/SETI_TimeSeries/DATA/ts_params.txt"
+data2 = "/Users/Jason/Desktop/SETI_TimeSeries/DATA/ts_params_nonsquiggle.txt"
 
 def parseData(filename, datapts, losses, ids):
     f = open(filename)
@@ -41,20 +41,19 @@ for i, ts in enumerate(temp):
     #raw_mean.append(np.mean(ts))
     raw_variance.append(np.var(ts))
 
-'''
+arima = []
 # Extract arima(1,1,1) params
 f = open(data2)
 f.readline()
 for line in f:
     line = line.strip().split(",")
     arima.append([float(line[1]) if line[1] != 'NA' else 0.0, float(line[2]) if line[2] != 'NA' else 0.0])
-'''
 
 #Ouput features as ts_dataset.csv
 output_file = open("official_ts_dataset_nonsquiggle.csv", 'w')
-output_file.write("\"id\",\"loss\",{0}\n".format(",".join(["\"X" + str(x) + "\"" for x in range(1,130)])))
+output_file.write("\"id\",\"loss\",\"AR\",\"MA\",\"raw_variance\",{0}\n".format(",".join(["\"X" + str(x) + "\"" for x in range(1,130)])))
 for i, ID in enumerate(ids):
-  output_file.write("\"{0}\",{1},{2}\n".format(ID, losses[i],",".join([str(x) for x in list(datapts[i])])))
+  output_file.write("\"{0}\",{1},{2},{3},{4},{5}\n".format(ID, losses[i], arima[i][0], arima[i][1], raw_variance[i],",".join([str(x) for x in list(datapts[i])])))
 
 output_file.close()
 
@@ -66,8 +65,8 @@ fTransformed = fTransformed[:,1:n//2]
 
 #Ouput features as ts_dataset.csv
 output_file = open("official_ts_dataset_nonsquiggle_dft.csv", 'w')
-output_file.write("\"id\",\"loss\",{0}\n".format(",".join(["\"X" + str(x) + "\"" for x in range(1,n//2)])))
+output_file.write("\"id\",\"loss\",\"AR\",\"MA\",\"raw_variance\",{0}\n".format(",".join(["\"X" + str(x) + "\"" for x in range(1,n//2)])))
 for i, ID in enumerate(ids):
-  output_file.write("\"{0}\",{1},{2}\n".format(ID, losses[i],",".join([str(x) for x in list(fTransformed[i])])))
+  output_file.write("\"{0}\",{1},{2},{3},{4},{5}\n".format(ID, losses[i], arima[i][0], arima[i][1], raw_variance[i],",".join([str(x) for x in list(fTransformed[i])])))
 
 output_file.close()
